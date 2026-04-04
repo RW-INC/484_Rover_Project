57f0486f083e800c7c3636c2002ae6ffc158e193
Krish Sridhar <skrish18@umd.edu>
2026-04-04 16:19:58 -0400

    secondary commit

--- Watchdog/main.cpp
+++ Watchdog/main.cpp
@@ -4,15 +4,7 @@
 
 int main() {
     std::cout << "--- SPARX Windows Test ---" << std::endl;
-
-    // Test a large vector to simulate high RAM usage
     std::vector<int> map_data(2000 * 2000, 0); 
     std::cout << "Allocated " << map_data.size() << " nodes successfully." << std::endl;
-
-    // TODO: Initialize your DStarLite object here
-    // DStarLite planner;
-    // planner.update_cell(5, 5, 10.0);
-
-    std::cout << "Logic test complete. System Nominal." << std::endl;
     return 0;
 }
